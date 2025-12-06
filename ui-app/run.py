9aca48de5b5c48b567cb1f426afc1a057dcb070c
MohammedBelfellah <bmwb6283@gmail.com>
2025-12-06 17:49:42 +0100

    feat: Update run.py to set default port to 5000 and enable debug mode for local development

--- ui-app/run.py
+++ ui-app/run.py
@@ -4,6 +4,16 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Get the PORT Railway assigns, default to 8000 if not set
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))  # Railway sets PORT, default to 5000 locally
+    app.run(host='0.0.0.0', port=port, debug=True)  # debug=True is fine locally
+
+
+
+
+
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
